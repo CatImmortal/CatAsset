e0449b1253d36d356a7c2e81e6c29723ca2c5684
CatImmortal <1304085906@qq.com>
2022-05-18 01:33:03 +0800

    增加资源清单相关类

---
+++ Assets/CatAsset/Runtime/Manifest/BundleManifestInfo.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatAsset
+{
+    /// <summary>
+    /// Bundle清单信息
+    /// </summary>
+    public class BundleManifestInfo
+    {
+        /// <summary>
+        /// 相对路径
+        /// </summary>
+        public string RelativePath;
+        
+        /// <summary>
+        /// 目录名
+        /// </summary>
+        public string DirectoryName;
+        
+        /// <summary>
+        /// 资源包名
+        /// </summary>
+        public string BundleName;
+
+        /// <summary>
+        /// 资源组
+        /// </summary>
+        public string Group;
+
+        /// <summary>
+        /// 是否为原生资源包
+        /// </summary>
+        public bool IsRaw;
+        
+        /// <summary>
+        /// 是否为场景资源包
+        /// </summary>
+        public bool IsScene;
+
+        /// <summary>
+        /// 文件长度
+        /// </summary>
+        public long Length;
+
+        /// <summary>
+        /// 文件Hash
+        /// </summary>
+        public Hash128 Hash;
+
+        /// <summary>
+        /// 资源清单信息列表
+        /// </summary>
+        public List<AssetManifestInfo> Assets = new List<AssetManifestInfo>();
+
+        public bool Equals(BundleManifestInfo other)
+        {
+            return RelativePath == other.RelativePath && Length == other.Length && Hash == other.Hash && Group == other.Group;
+        }
+    }
+}
+
---
+++ Assets/CatAsset/Runtime/Manifest/CatAssetManifest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatAsset
+{
+    /// <summary>
+    /// CatAsset资源清单
+    /// </summary>
+    public class CatAssetManifest
+    {
+        /// <summary>
+        /// 游戏版本号
+        /// </summary>
+        public string GameVersion;
+
+        /// <summary>
+        /// 清单版本号
+        /// </summary>
+        public int ManifestVersion;
+
+        /// <summary>
+        /// 资源包清单信息列表
+        /// </summary>
+        public List<BundleManifestInfo> Bundles = new List<BundleManifestInfo>();
+    }
+}
+
