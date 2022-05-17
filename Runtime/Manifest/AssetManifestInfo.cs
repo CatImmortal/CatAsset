fd5c19d4f2b4a550e789e2bf37e32558556575fc
CatImmortal <1304085906@qq.com>
2022-05-18 01:33:03 +0800

    增加资源清单相关类

---
+++ Runtime/Manifest/AssetManifestInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatAsset
+{
+    /// <summary>
+    /// 资源清单信息
+    /// </summary>
+    public class AssetManifestInfo
+    {
+        /// <summary>
+        /// 资源名
+        /// </summary>
+        public string AssetName;
+
+        /// <summary>
+        /// 依赖资源名列表
+        /// </summary>
+        public List<string> Dependencies;
+    }
+
+}
