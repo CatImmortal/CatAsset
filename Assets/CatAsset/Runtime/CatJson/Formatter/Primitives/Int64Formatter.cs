896342132bca4832cfb525a7074ed859cd3b7aa6
CatImmortal <1304085906@qq.com>
2022-05-18 01:31:15 +0800

    提交CatJson库

---
+++ Assets/CatAsset/Runtime/CatJson/Formatter/Primitives/Int64Formatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CatJson
+{
+    /// <summary>
+    /// long类型的Json格式化器
+    /// </summary>
+    public class Int64Formatter : BaseJsonFormatter<long>
+    {
+        /// <inheritdoc />
+        public override void ToJson(long value, Type type, Type realType, int depth)
+        {
+            TextUtil.Append(value.ToString());
+        }
+
+        /// <inheritdoc />
+        public override long ParseJson(Type type, Type realType)
+        {
+            RangeString rs = JsonParser.Lexer.GetNextTokenByType(TokenType.Number);
+            return long.Parse(rs.ToString());
+        }
+    }
+}
---
+++ Assets/CatAsset/Runtime/CatJson/Formatter/Unity/Hash128Formatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace CatJson
+{
+    /// <summary>
+    /// Hash128类型的Json格式化器
+    /// </summary>
+    public class Hash128Formatter : BaseJsonFormatter<Hash128>
+    {
+        /// <inheritdoc />
+        public override void ToJson(Hash128 value, Type type, Type realType, int depth)
+        {
+            TextUtil.Append($"\"{value.ToString()}\"");
+        }
+
+        /// <inheritdoc />
+        public override Hash128 ParseJson(Type type, Type realType)
+        {
+            RangeString rs = JsonParser.Lexer.GetNextTokenByType(TokenType.String);
+            return Hash128.Parse(rs.ToString());
+        }
+    }
+}
