6d198242557711b19bc458f6763d1fdd34deb7ff
CatImmortal <1304085906@qq.com>
2022-06-27 01:09:57 +0800

    [init]:添加Entry场景

--- Assets/Scripts/Entry.cs
+++ Assets/Scripts/Entry.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CatAsset.Runtime;
+using UnityEngine.SceneManagement;
 
 public class Entry : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public Scene scene1;
+    public Scene scene2;
+    public Scene scene3;
     void Start()
     {
         CatAssetManager.CheckPackageManifest((b =>
@@ -14,25 +18,39 @@ public class Entry : MonoBehaviour
         }));
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            CatAssetManager.LoadAsset<GameObject>("Assets/BundleRes/Prefab2/TestGO 1.prefab",null,((success, asset, userdata) =>
-            {
-           
-            }));
+            //SceneManager.UnloadSceneAsync("Assets/BundleRes/Scene/TestScene.unity");
+            CatAssetManager.LoadAsset("Assets/BundleRes/PrefabA/A1.prefab",null,null);
+            CatAssetManager.LoadAsset("Assets/BundleRes/PrefabA/A1.prefab",null,null);
+            CatAssetManager.LoadAsset("Assets/BundleRes/PrefabA/A1.prefab",null,null);
+
         }
-        
-        if (Input.GetKeyDown(KeyCode.D))
+
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            CatAssetManager.LoadAsset<Texture2D>("Assets/BundleRes/Texture/Tex1.jpg",null,((success, asset, userdata) =>
-            {
-           
-            }));
+
         }
+
+        
     }
 
 
+
+}
+
+public class A
+{
+    private int b;
+
+    public void Test(A a)
+    {
+        b = 1;
+        a.b = 1;
+    }
 }
