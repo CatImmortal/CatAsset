395d9dad1521b1730ed2576560de862921507bb6
CatImmortal <1304085906@qq.com>
2022-07-04 23:58:15 +0800

    [feat][runtime]:增加资源生命周期绑定接口，以提供一定程度的自动资源卸载

---
+++ Assets/CatAsset/Runtime/GameObjectExtension.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace CatAsset.Runtime
+{
+    public static class GameObjectExtension
+    {
+        /// <summary>
+        /// 获取组件，若不存在则添加
+        /// </summary>
+        public static T GetOrAddComponent<T>(this GameObject go) where T: Component
+        {
+            T component = go.GetComponent<T>();
+            if (component == null)
+            {
+                component = go.AddComponent<T>();
+            }
+
+            return component;
+        }
+    }
+}
