2e6fe648eb3a7f7937f822117d9cad6480ffb31d
CatImmortal <1304085906@qq.com>
2022-07-14 02:07:03 +0800

    [feat][editor]:资源包构建完全SBP化

---
+++ Assets/CatAsset/Editor/Pipeline/ContextObject/ManifestParam.cs
@@ -0,0 +1,19 @@
+﻿using CatAsset.Runtime;
+
+namespace CatAsset.Editor
+{
+    /// <inheritdoc />
+    public class ManifestParam : IManifestParam
+    {
+        public CatAssetManifest Manifest { get; }
+        public string WritePath { get; }
+
+        public ManifestParam(CatAssetManifest manifest, string writePath)
+        {
+            Manifest = manifest;
+            WritePath = writePath;
+        }
+        
+        
+    }
+}
