48078c465a0f9442ac5ac43730253e677909d0d0
CatImmortal <1304085906@qq.com>
2022-10-24 00:53:03 +0800

    [feat][editor]:增加对构建目录与资源的特殊显示

---
+++ Assets/CatAsset/Editor/Misc/DrawDescTool.cs
@@ -0,0 +1,79 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CatAsset.Editor
+{
+    /// <summary>
+    /// 绘制Project窗口下，文件/文件夹描述信息的工具类
+    /// </summary>
+    public static class DrawDescTool
+    {
+        private static Color dirColor = Color.gray;
+        private static Color assetColor = new Color(0, 0.635f, 0.635f);
+        
+        [InitializeOnLoadMethod]
+        private static void InitializeOnLoadMethod()
+        {
+            EditorApplication.projectWindowItemOnGUI += OnGUI;
+        }
+
+        private static void OnGUI(string guid, Rect selectionRect)
+        {
+
+            if (BundleBuildConfigSO.Instance == null)
+            {
+                return;
+            }
+
+            if (BundleBuildConfigSO.Instance.Directories.Count > 0 && BundleBuildConfigSO.Instance.DirectoryDict.Count == 0)
+            {
+                BundleBuildConfigSO.Instance.RefreshDict();
+            }
+
+            
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            if (AssetDatabase.IsValidFolder(path))
+            {
+                //是目录
+                if (BundleBuildConfigSO.Instance.DirectoryDict.TryGetValue(path,out BundleBuildDirectory bbd))
+                {
+                    //绘制资源组在文件夹后面
+                    string desc = bbd.Group;
+                    DrawDesc(desc,selectionRect,dirColor);
+                }
+
+            }
+            else
+            {
+                if (BundleBuildConfigSO.Instance.AssetToBundleDict.TryGetValue(path,out BundleBuildInfo bbi))
+                {
+                    //绘制资源包相对路径在文件后面
+                    string desc = bbi.RelativePath;
+                    DrawDesc(desc,selectionRect,assetColor);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 绘制Project面板下文件/文件夹后的描述信息
+        /// </summary>
+        private static void DrawDesc(string desc,Rect selectionRect,Color descColor)
+        {
+
+            GUIStyle label = EditorStyles.label;
+            GUIContent content = new GUIContent(desc);
+            float width = label.CalcSize(content).x + 10;
+                        
+            Rect pos = selectionRect;
+            pos.x = pos.xMax - width;  //绘制在最右边
+            pos.width = width;
+            pos.yMin++;
+                        
+            Color color = GUI.color;
+            GUI.color = descColor; //
+            GUI.DrawTexture(pos, EditorGUIUtility.whiteTexture);
+            GUI.color = color;
+            GUI.Label(pos, desc);
+        }
+    }
+}
