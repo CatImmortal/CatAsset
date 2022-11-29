399597e006f24306b4e6e896f7bda81f3ed74887
CatImmortal <1304085906@qq.com>
2022-11-29 18:31:25 +0800

    [refactor][editor]:使用TreeView重构分析器窗口

--- Editor/Window/ProfilerInfoWindow/ProfilerInfoWindow.BundleInfo.cs
+++ Editor/Window/ProfilerInfoWindow/ProfilerInfoWindow.BundleInfo.cs
@@ -3,24 +3,47 @@ using System.Collections.Generic;
 using System.Reflection;
 using CatAsset.Runtime;
 using UnityEditor;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 namespace CatAsset.Editor
 {
     public partial class ProfilerInfoWindow
     {
-
-        private Vector2 scrollPos;
-
-        /// <summary>
-        /// 是否只显示主动加载的资源
-        /// </summary>
-        private bool isOnlyShowActiveLoad = false;
+        private TreeViewState bundleInfoTreeViewState;
+        private BundleInfoTreeView bundleInfoTreeView;
 
         /// <summary>
-        /// 资源包相对路径->是否展开
+        /// 初始化资源包信息树视图
         /// </summary>
-        private Dictionary<string, bool> foldOutDict = new Dictionary<string, bool>();
+        private void InitBundleInfoTreeView()
+        {
+            List<string> columnList = new List<string>()
+            {
+                "名称",
+                "Object",
+                "资源组",
+                "内存中资源数",
+                "引用中资源数",
+                "长度",
+                "引用计数",
+                "上游节点数",
+                "下游节点数",
+                "查看依赖关系图",
+            };
+
+            var columns = CreateColumns(columnList);
+            columns[0].minWidth = 400;
+
+            var state = new MultiColumnHeaderState(columns);
+
+            var header = new MultiColumnHeader(state);
+            header.ResizeToFit();
+
+            bundleInfoTreeViewState = new TreeViewState();
+            bundleInfoTreeView = new BundleInfoTreeView(bundleInfoTreeViewState, header);
+        }
+
 
 
         /// <summary>
@@ -28,175 +51,15 @@ namespace CatAsset.Editor
         /// </summary>
         private void DrawBundleInfoView()
         {
-            bool isAllFoldOutTrue = false;
-            bool isAllFoldOutFalse = false;
-
-            using (new EditorGUILayout.HorizontalScope())
+            if (profilerInfo == null ||  profilerInfo.BundleInfoList.Count == 0)
             {
-                if (GUILayout.Button("全部展开", GUILayout.Width(100)))
-                {
-                    isAllFoldOutTrue = true;
-                }
-
-                if (GUILayout.Button("全部收起", GUILayout.Width(100)))
-                {
-                    isAllFoldOutFalse = true;
-                }
-
-                isOnlyShowActiveLoad = GUILayout.Toggle(isOnlyShowActiveLoad, "只显示主动加载的资源", GUILayout.Width(200));
+                return;
             }
 
-            using (EditorGUILayout.ScrollViewScope sv = new EditorGUILayout.ScrollViewScope(scrollPos))
-            {
-                scrollPos = sv.scrollPosition;
-
-                if (profilerInfo == null)
-                {
-                    return;
-                }
-
-                foreach (var profilerBundleInfo in profilerInfo.BundleInfoList)
-                {
-                    string relativePath = profilerBundleInfo.RelativePath;
-
-                    if (!foldOutDict.ContainsKey(relativePath))
-                    {
-                        foldOutDict.Add(relativePath, false);
-                    }
-
-                    if (isAllFoldOutTrue)
-                    {
-                        //点击过全部展开
-                        foldOutDict[relativePath] = true;
-                    }
-                    else if (isAllFoldOutFalse)
-                    {
-                        //点击过全部收起
-                        foldOutDict[relativePath] = false;
-                    }
-
-                    if (isOnlyShowActiveLoad)
-                    {
-                        //仅显示主动加载的资源
-                        //此资源包至少有一个主动加载的资源，才能显示
-                        bool canShow = false;
-                        foreach (var profilerAssetInfo in profilerBundleInfo.InMemoryAssets)
-                        {
-                            if (profilerAssetInfo.RefCount != profilerAssetInfo.DependencyChain.DownStream.Count)
-                            {
-                                canShow = true;
-                                break;
-                            }
-                        }
-                        if (!canShow)
-                        {
-                            continue;
-                        }
-                    }
-
-                    using (new EditorGUILayout.HorizontalScope())
-                    {
-                        foldOutDict[relativePath] = EditorGUILayout.Foldout(foldOutDict[relativePath], relativePath);
-
-                        DrawProfilerBundleInfo(profilerBundleInfo,false);
-                    }
-
-                    if (foldOutDict[relativePath])
-                    {
-                        EditorGUILayout.Space();
-
-                        foreach (var profilerAssetInfo in profilerBundleInfo.InMemoryAssets)
-                        {
-                            if (isOnlyShowActiveLoad)
-                            {
-                                if (profilerAssetInfo.RefCount == profilerAssetInfo.DependencyChain.DownStream.Count)
-                                {
-                                    //只显示主动加载的资源 且 此资源是纯被依赖加载的 就跳过
-                                    continue;
-                                }
-
-                            }
-
-                            DrawProfilerAssetInfo(profilerAssetInfo);
-                        }
-
-                        EditorGUILayout.Space();
-                    }
-                }
-            }
-
-
+            bundleInfoTreeView.OnGUI(new Rect(0, 60, position.width, position.height));
         }
 
-        /// <summary>
-        /// 绘制分析器资源包信息
-        /// </summary>
-        private void DrawProfilerBundleInfo(ProfilerBundleInfo profilerBundleInfo,bool isDrawName = true)
-        {
-            using (new EditorGUILayout.HorizontalScope())
-            {
-                if (isDrawName)
-                {
-                    EditorGUILayout.LabelField($"{profilerBundleInfo.RelativePath}" ,GUILayout.Width(400));
-                }
-                else
-                {
-                    EditorGUILayout.Space();
-                }
 
-                EditorGUILayout.LabelField($"资源组：{profilerBundleInfo.Group}" ,GUILayout.Width(150));
-                EditorGUILayout.LabelField($"内存中资源数：{profilerBundleInfo.InMemoryAssets.Count}/{profilerBundleInfo.TotalAssetCount}",GUILayout.Width(125));
-                EditorGUILayout.LabelField($"引用中资源数：{profilerBundleInfo.ReferencingAssetCount}/{profilerBundleInfo.TotalAssetCount}" ,GUILayout.Width(125));
-                EditorGUILayout.LabelField($"文件长度：{RuntimeUtil.GetByteLengthDesc(profilerBundleInfo.Length)}",GUILayout.Width(125));
-
-                EditorGUILayout.LabelField($"上游资源包数：{profilerBundleInfo.DependencyChain.UpStream.Count}",GUILayout.Width(125));
-                EditorGUILayout.LabelField($"下游资源包数：{profilerBundleInfo.DependencyChain.DownStream.Count}",GUILayout.Width(125));
-
-
-                if (GUILayout.Button("查看资源包依赖关系图") && (profilerBundleInfo.DependencyChain.UpStream.Count > 0 || profilerBundleInfo.DependencyChain.DownStream.Count > 0))
-                {
-                    DependencyGraphViewWindow.Open<ProfilerBundleInfo,BundleNode>(profilerBundleInfo);
-                }
-
-                EditorGUILayout.LabelField("", GUILayout.Width(30));
-            }
-
-
-        }
-
-        /// <summary>
-        /// 绘制分析器资源信息
-        /// </summary>
-        private void DrawProfilerAssetInfo(ProfilerAssetInfo profilerAssetInfo)
-        {
-            using (new EditorGUILayout.HorizontalScope())
-            {
-                //资源名
-                EditorGUILayout.LabelField("\t" + profilerAssetInfo.Name);
-
-                //对象引用
-                EditorGUI.BeginDisabledGroup(true);
-                EditorGUILayout.ObjectField(AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(profilerAssetInfo.Name), typeof(UnityEngine.Object),false);
-                EditorGUI.EndDisabledGroup();
-
-                //长度
-                EditorGUILayout.LabelField($"\t长度：{RuntimeUtil.GetByteLengthDesc(profilerAssetInfo.Length)}");
-
-                //引用计数
-                EditorGUILayout.LabelField($"\t引用计数：{profilerAssetInfo.RefCount}");
-
-                //上游资源数
-                EditorGUILayout.LabelField($"\t上游资源数：{profilerAssetInfo.DependencyChain.UpStream.Count}");
-
-                //下游资源数
-                EditorGUILayout.LabelField($"\t下游资源数：{profilerAssetInfo.DependencyChain.DownStream.Count}");
-
-                if (GUILayout.Button("查看资源依赖关系图") && (profilerAssetInfo.DependencyChain.UpStream.Count > 0 || profilerAssetInfo.DependencyChain.DownStream.Count > 0))
-                {
-                    DependencyGraphViewWindow.Open<ProfilerAssetInfo,AssetNode>(profilerAssetInfo);
-                }
-            }
-        }
     }
 }
 
---
+++ Editor/Window/ProfilerInfoWindow/TreeViewDataItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor.IMGUI.Controls;
+
+namespace CatAsset.Editor
+{
+    /// <summary>
+    /// 树视图数据项
+    /// </summary>
+    public class TreeViewDataItem<T> : TreeViewItem
+    {
+        public T Data;
+    }
+}
