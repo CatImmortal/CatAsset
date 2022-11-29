e4bd9e386b8c19120249cade112f04dd99421a1f
CatImmortal <1304085906@qq.com>
2022-11-29 11:29:48 +0800

    [refactor][runtime]:VersionCheckResult增加GroupUpdateList字段

--- Assets/CatAsset/Runtime/Updatable/Checker/VersionCheckResult.cs
+++ Assets/CatAsset/Runtime/Updatable/Checker/VersionCheckResult.cs
@@ -1,30 +1,42 @@
-﻿namespace CatAsset.Runtime
+﻿using System.Collections.Generic;
+
+namespace CatAsset.Runtime
 {
     /// <summary>
     /// 版本检查结果
     /// </summary>
-    public struct VersionCheckResult
+    public readonly struct VersionCheckResult
     {
         /// <summary>
         /// 检查失败时的异常信息
         /// </summary>
-        public string Error;
+        public readonly string Error;
         
         /// <summary>
         /// 需要更新的资源包总数量
         /// </summary>
-        public int TotalCount;
+        public readonly int TotalCount;
             
         /// <summary>
         /// 需要更新的资源包总长度
         /// </summary>
-        public ulong TotalLength;
+        public readonly ulong TotalLength;
+
+        /// <summary>
+        /// 资源组更新器列表
+        /// </summary>
+        public readonly List<GroupUpdater> GroupUpdaters;
 
         public VersionCheckResult(string error,int totalCount, ulong totalLength)
         {
             Error = error;
             TotalCount = totalCount;
             TotalLength = totalLength;
+            GroupUpdaters = new List<GroupUpdater>(CatAssetUpdater.GroupUpdaterDict.Count);
+            foreach (var pair in CatAssetUpdater.GroupUpdaterDict)
+            {
+                GroupUpdaters.Add(pair.Value);
+            }
         }
 
         public override string ToString()
