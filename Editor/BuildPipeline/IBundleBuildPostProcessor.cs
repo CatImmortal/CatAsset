38be151ae101e241f3e1ad679b16cad6d40c1a7d
CatImmortal <1304085906@qq.com>
2023-01-16 14:47:59 +0800

    [typo]:修改一些命名

--- Editor/BuildPipeline/IBundleBuildPostProcessor.cs
+++ Editor/BuildPipeline/IBundleBuildPostProcessor.cs
@@ -4,12 +4,15 @@ using UnityEditor.Build.Pipeline.Interfaces;
 
 namespace CatAsset.Editor
 {
-    public interface IPostProcessBuildBundles
+    /// <summary>
+    /// 资源包构建后的处理回调接口
+    /// </summary>
+    public interface IBundleBuildPostProcessor
     {
         /// <summary>
         /// 构建资源包后调用
         /// </summary>
-        void OnPostProcessBuildBundles(BundleBuildConfigSO bundleBuildConfig, BuildTarget targetPlatform,string outputFolder,
+        void OnBundleBuildPostProcess(BundleBuildConfigSO bundleBuildConfig, BuildTarget targetPlatform,string outputFolder,
             ReturnCode returnCode, IBundleBuildResults result);
     }
 }
