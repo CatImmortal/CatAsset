c145057d3bbe82482fba57512309b3f8fd38beb3
CatImmortal <1304085906@qq.com>
2023-02-01 14:18:52 +0800

    [refactor]:重构分析器资源组信息窗口

--- Editor/Window/ProfilerInfoWindow/SubWindows/GroupInfo/GroupInfoTreeView.cs
+++ Editor/Window/ProfilerInfoWindow/SubWindows/GroupInfo/GroupInfoTreeView.cs
@@ -20,8 +20,12 @@ namespace CatAsset.Editor
         {
             Name,
 
+            LocalBundles,
+            State,
             LocalCount,
             LocalLength,
+            
+            RemoteBundles,
             RemoteCount,
             RemoteLength,
 
@@ -49,33 +53,53 @@ namespace CatAsset.Editor
             ColumnType column = (ColumnType)header.sortedColumnIndex;
 
             IOrderedEnumerable<ProfilerGroupInfo> groupOrdered = null;
+            IOrderedEnumerable<ProfilerGroupInfo.BundleInfo> bundleOrdered = null;
 
             switch (column)
             {
                 case ColumnType.Name:
                     groupOrdered = TreeViewData.GroupInfoList.Order(info => info.Name, ascending);
                     break;
-
+                
+                case ColumnType.State:
+                    foreach (var groupInfo in TreeViewData.GroupInfoList)
+                    {
+                        bundleOrdered = groupInfo.RemoteBundles.Order(info => info.State, ascending);
+                        groupInfo.RemoteBundles = new List<ProfilerGroupInfo.BundleInfo>(bundleOrdered);
+                    }
+                    Reload();
+                    break;
+                
                 case ColumnType.LocalCount:
                     groupOrdered = TreeViewData.GroupInfoList.Order(info => info.LocalCount, ascending);
                     break;
-
+                
                 case ColumnType.LocalLength:
                     groupOrdered = TreeViewData.GroupInfoList.Order(info => info.LocalLength, ascending);
                     break;
-
+                
+                case ColumnType.LocalBundles:
+                case ColumnType.RemoteBundles:
+                    foreach (var groupInfo in TreeViewData.GroupInfoList)
+                    {
+                        bundleOrdered = groupInfo.RemoteBundles.Order(info => info.Name, ascending);
+                        groupInfo.RemoteBundles = new List<ProfilerGroupInfo.BundleInfo>(bundleOrdered);
+                    }
+                    Reload();
+                    break;
+                
                 case ColumnType.RemoteCount:
                     groupOrdered = TreeViewData.GroupInfoList.Order(info => info.RemoteCount, ascending);
                     break;
-
+                
                 case ColumnType.RemoteLength:
                     groupOrdered = TreeViewData.GroupInfoList.Order(info => info.RemoteLength, ascending);
                     break;
-
+                
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-
+            
             if (groupOrdered != null)
             {
                 TreeViewData.GroupInfoList = new List<ProfilerGroupInfo>(groupOrdered);
@@ -96,6 +120,15 @@ namespace CatAsset.Editor
                 {
                     id = groupInfo.Name.GetHashCode(), displayName = groupInfo.Name, Data = groupInfo,
                 };
+
+                foreach (ProfilerGroupInfo.BundleInfo info in groupInfo.RemoteBundles)
+                {
+                    var bundleNode = new TreeViewDataItem<ProfilerGroupInfo.BundleInfo>()
+                    {
+                        id = info.Name.GetHashCode(), displayName = info.Name, Data = info,
+                    };
+                    groupNode.AddChild(bundleNode);
+                }
 
                 root.AddChild(groupNode);
             }
@@ -118,41 +151,87 @@ namespace CatAsset.Editor
         /// </summary>
         private void CellGUI(Rect cellRect, TreeViewItem item, ColumnType column, ref RowGUIArgs args)
         {
-            TreeViewDataItem<ProfilerGroupInfo> groupItem = (TreeViewDataItem<ProfilerGroupInfo>)item;
+            var groupItem = item as TreeViewDataItem<ProfilerGroupInfo>;
+            var bundleItem = item as TreeViewDataItem<ProfilerGroupInfo.BundleInfo>;
             GUIStyle centerStyle = new GUIStyle() { alignment = TextAnchor.MiddleCenter };
             centerStyle.normal = new GUIStyleState(){textColor = Color.white};
+
             switch (column)
             {
                 case ColumnType.Name:
                     args.rowRect = cellRect;
-                    args.label = groupItem.Data.Name;
+                    if (groupItem != null)
+                    {
+                        args.label = groupItem.Data.Name;
+                    }
+                    else
+                    {
+                        args.label = string.Empty;
+                    }
                     base.RowGUI(args);
                     break;
-
+                
+                case ColumnType.LocalBundles:
+                    if (bundleItem != null && bundleItem.Data.State != BundleRuntimeInfo.State.InRemote)
+                    {
+                        EditorGUI.LabelField(cellRect,bundleItem.Data.Name,centerStyle);
+                    }
+                    break;
+                
+                case ColumnType.State:
+                    if (bundleItem != null && bundleItem.Data.State != BundleRuntimeInfo.State.InRemote)
+                    {
+                        EditorGUI.LabelField(cellRect,bundleItem.Data.State.ToString(),centerStyle);
+                    }
+                    break;
+                
                 case ColumnType.LocalCount:
-                    if (GUI.Button(cellRect, groupItem.Data.LocalCount.ToString()))
+                    if (groupItem != null)
                     {
-                       GroupBundleListWindow.Open($"{groupItem.Data.Name}组本地资源包列表",groupItem.Data.LocalBundles);
+                        EditorGUI.LabelField(cellRect,groupItem.Data.LocalCount.ToString(),centerStyle);
                     }
                     break;
-
+                
                 case ColumnType.LocalLength:
-                    EditorGUI.LabelField(cellRect,RuntimeUtil.GetByteLengthDesc(groupItem.Data.LocalLength),centerStyle);
-                    break;
-
-                case ColumnType.RemoteCount:
-                    if (GUI.Button(cellRect, groupItem.Data.RemoteCount.ToString()))
+                    ulong length = 0;
+                    if (groupItem != null)
                     {
-                        GroupBundleListWindow.Open($"{groupItem.Data.Name}组远端资源包列表",groupItem.Data.RemoteBundles);
+                        length = groupItem.Data.LocalLength;
+                    }
+                    else
+                    {
+                        length = bundleItem.Data.Length;
+                    }
+                    EditorGUI.LabelField(cellRect,RuntimeUtil.GetByteLengthDesc(length),centerStyle);
+                    break;
+                
+                case ColumnType.RemoteBundles:
+                    if (bundleItem != null)
+                    {
+                        EditorGUI.LabelField(cellRect,bundleItem.Data.Name,centerStyle);
                     }
                     break;
-
-                case ColumnType.RemoteLength:
-                    EditorGUI.LabelField(cellRect,RuntimeUtil.GetByteLengthDesc(groupItem.Data.RemoteLength),centerStyle);
+                
+                case ColumnType.RemoteCount:
+                    if (groupItem != null)
+                    {
+                        EditorGUI.LabelField(cellRect,groupItem.Data.RemoteCount.ToString(),centerStyle);
+                    }
                     break;
-
-
-
+                
+                case ColumnType.RemoteLength:
+                    length = 0;
+                    if (groupItem != null)
+                    {
+                        length = groupItem.Data.RemoteLength;
+                    }
+                    else
+                    {
+                        length = bundleItem.Data.Length;
+                    }
+                    EditorGUI.LabelField(cellRect,RuntimeUtil.GetByteLengthDesc(length),centerStyle);
+                    break;
+                
                 default:
                     throw new ArgumentOutOfRangeException(nameof(column), column, null);
             }
