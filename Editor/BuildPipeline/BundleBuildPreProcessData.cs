aea7f07a8623fcad44ed5f3dcc05722255506d86
CatImmortal <1304085906@qq.com>
2023-03-07 17:28:33 +0800

    [refactor][editor]:重构资源包构建回调相关代码

---
+++ Editor/BuildPipeline/BundleBuildPreProcessData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+namespace CatAsset.Editor
+{
+    /// <summary>
+    /// 资源包构建预处理数据
+    /// </summary>
+    public class BundleBuildPreProcessData
+    {
+        /// <summary>
+        /// 构建配置
+        /// </summary>
+        public BundleBuildConfigSO Config;
+        
+        /// <summary>
+        /// 目标平台
+        /// </summary>
+        public BuildTarget TargetPlatform;
+    }
+}
--- Editor/BuildPipeline/IBundleBuildPreProcessor.cs
+++ Editor/BuildPipeline/IBundleBuildPreProcessor.cs
@@ -3,13 +3,13 @@
 namespace CatAsset.Editor
 {
     /// <summary>
-    /// 资源包构建前的处理回调接口
+    /// 资源包构建前的预处理回调接口
     /// </summary>
     public interface IBundleBuildPreProcessor
     {
         /// <summary>
         /// 构建资源包前调用
         /// </summary>
-        void OnBundleBuildPreProcess(BundleBuildConfigSO bundleBuildConfig, BuildTarget targetPlatform);
+        void OnBundleBuildPreProcess(BundleBuildPreProcessData data);
     }
 }
