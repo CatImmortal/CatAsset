756c859480571ab6660d4c7e308359a80e1c4756
CatImmortal <1304085906@qq.com>
2023-09-01 09:50:47 +0800

    [feat][editor]:排除ShaderGraph文件

--- Assets/CatAsset/Editor/Misc/EditorUtil.cs
+++ Assets/CatAsset/Editor/Misc/EditorUtil.cs
@@ -30,6 +30,8 @@ namespace CatAsset.Editor
             ".so",
             ".dll",
             ".cginc",
+            ".shadergraph",
+            ".shadersubgraph",
         };
 
         /// <summary>
